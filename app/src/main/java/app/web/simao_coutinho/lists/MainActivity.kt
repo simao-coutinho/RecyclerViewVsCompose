a180e2cd575eb366f1e0e4b8809b7c6a52cb8b13
SimaoCoutinho <simaopedroucoutinho@gmail.com>
2025-01-02 15:09:20 +0000

    wip

--- app/src/main/java/app/web/simao_coutinho/lists/MainActivity.kt
+++ app/src/main/java/app/web/simao_coutinho/lists/MainActivity.kt
@@ -1,20 +1,36 @@
 package app.web.simao_coutinho.lists
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import app.web.simao_coutinho.data.Product
+import app.web.simao_coutinho.lists.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val products = Product.getProducts()
+
+        binding.rvProducts.layoutManager = LinearLayoutManager(this)
+        binding.rvProducts.setHasFixedSize(true)
+
     }
 }
---
+++ app/src/main/java/app/web/simao_coutinho/lists/ProductAdapter.kt
@@ -0,0 +1,8 @@
+package app.web.simao_coutinho.lists
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductAdapter {
+
+
+}
